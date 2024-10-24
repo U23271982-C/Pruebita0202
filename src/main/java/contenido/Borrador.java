96efab7a29a988a585eeef0c17336e9b92216530
= <u23271982@utp.edu.pe>
2024-10-24 17:21:55 -0500

    Borrando todo y agregando la clase Borrador

---
+++ src/main/java/contenido/Borrador.java
@@ -0,0 +1,4 @@
+package contenido;
+
+public class Borrador {
+}
--- src/main/java/contenido/J02.java
+++
@@ -1,7 +0,0 @@
-package contenido;
-
-public class J02 {
-    public static void main(String[] args) {
-        System.out.println("Hola putita");
-    }
-}
